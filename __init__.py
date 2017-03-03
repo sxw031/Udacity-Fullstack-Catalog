e28cc5a0ab6cb038589450264293315dab6b3e8b
sxw031 <jameswangishere@gmail.com>
2017-03-02 19:49:13 -0800

    delete unneccessary files

--- __init__.py
+++ __init__.py
@@ -496,7 +496,7 @@ def categoryItemsJSON(category_id):
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
 	app.debug = True
-	app.run(host='127.0.0.1', port=8080)
+	app.run(host='127.0.0.1', port=8888)
 
 
 
