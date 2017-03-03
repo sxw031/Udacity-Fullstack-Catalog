599374674c40e938c95a2a1d73ea2fccd239a34a
sxw031 <jameswangishere@gmail.com>
2017-03-02 18:02:11 -0800

    fix the database engine information and correct json path

--- application.py
+++ application.py
@@ -19,12 +19,12 @@ from functools import wraps
 
 app = Flask(__name__)
 
-CLIENT_ID = json.loads(open('g_client_secrets.json','r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('/var/www/catalog/catalog/g_client_secrets.json','r').read())['web']['client_id']
 APPLICATION_NAME = "Fun Catalog"
 
 ########################### Connect to Databse and Create Session #################################
 
-engine = create_engine('postgresql://datauser1:super123@localhost/catalogdb')
+engine = create_engine('postgresql://postgres:happy123@localhost/catalogdb')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
@@ -150,8 +150,8 @@ def fbconnect():
 	access_token = data['access_token']
 	# print "access token received %s " % access_token
 	
-	app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
-	app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+	app_id = json.loads(open('/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())['web']['app_id']
+	app_secret = json.loads(open('var/www/catalog/catalog/fb_client_secrets.json', 'r').read())['web']['app_secret']
 	url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
 	h = httplib2.Http()
 	result = h.request(url, 'GET')[1]
